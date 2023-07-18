6bb3a471a334ac0eca4257cf0d05d678d280f028
Horaiz-UQ <ekzmvlfxj@naver.com>
2023-07-18 13:45:41 +0900

    init: 초기 설정

---
+++ src/test/java/team7/example/Toyproject3/ToyProject3ApplicationTests.java
@@ -0,0 +1,13 @@
+package team7.example.Toyproject3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProject3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
