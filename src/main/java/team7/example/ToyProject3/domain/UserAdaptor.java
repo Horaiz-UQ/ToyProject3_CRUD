26a20a61ea670d50a2cd82c14309587c23e54257
Horaiz-UQ <ekzmvlfxj@naver.com>
2023-07-18 13:55:52 +0900

    feat: UserAdaptor 추가

---
+++ src/main/java/team7/example/ToyProject3/domain/UserAdaptor.java
@@ -0,0 +1,20 @@
+package team7.example.ToyProject3.domain;
+
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Getter
+public class UserAdaptor extends User implements Serializable {
+
+    private team7.example.ToyProject3.domain.User user;
+
+    public UserAdaptor(team7.example.ToyProject3.domain.User user, List<GrantedAuthority> authorities){
+        super(user.getEmail(),user.getPassword(), authorities);
+        this.user = user;
+    }
+
+}
