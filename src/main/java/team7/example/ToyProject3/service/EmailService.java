19b93a1ff177450d17e5af85fe7b26b9667eba30
Horaiz-UQ <ekzmvlfxj@naver.com>
2023-07-20 13:22:57 +0900

    feat: 이메일 기능 추가

---
+++ src/main/java/team7/example/ToyProject3/service/EmailService.java
@@ -0,0 +1,40 @@
+package team7.example.ToyProject3.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import team7.example.ToyProject3.domain.user.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Slf4j
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public boolean sendSimpleText(User userMail) {
+
+        List<String> receivers = new ArrayList<>();
+
+        receivers.add(userMail.getEmail());
+
+        String[] arrReceiver = (String[])receivers.toArray(new String[receivers.size()]);
+
+
+        SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+
+        simpleMailMessage.setTo(arrReceiver);
+        simpleMailMessage.setSubject("회원 등급 변경 알림");
+        simpleMailMessage.setText(userMail.getName() + "님의 사용자 권한이 변경되었습니다." );
+
+        mailSender.send(simpleMailMessage);
+
+        return true;
+    }
+}
